9a27e03205ada9afd3f6d7f1b8b70b7a43973e7e
Gautam Trivedi <gtrivedi@gogoair.com>
2019-12-27 12:15:42 -0700

    forloop practice

---
+++ forloop/forloop.go
@@ -0,0 +1,12 @@
+package forloop
+
+import (
+	"fmt"
+)
+
+//ForloopPractice test for for loop
+func ForloopPractice() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
---
+++ forloopTest.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/gdtrivedi/gopractice/forloop"
+)
+
+func main() {
+	forloop.ForloopPractice()
+}
