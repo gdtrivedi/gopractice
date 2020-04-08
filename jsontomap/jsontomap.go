4af7c559fd0703c5e85659b3345a77b9b2801a04
Gautam Trivedi <gtrivedi@gogoair.com>
2020-04-08 15:46:07 -0600

    json to map

---
+++ jsontomap/jsontomap.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func dumpMap(space string, m map[string]interface{}) {
+	for k, v := range m {
+		if mv, ok := v.(map[string]interface{}); ok {
+			fmt.Printf("{ \"%v\": \n", k)
+			dumpMap(space+"\t", mv)
+			fmt.Printf("}\n")
+		} else {
+			fmt.Printf("%v %v : %v\n", space, k, v)
+		}
+	}
+}
+
+var jsonStr = `
+{
+  "array": [
+	1,
+	2,
+	3
+  ],
+  "boolean": true,
+  "null": null,
+  "number": 123,
+  "object": {
+	"a": "b",
+	"c": "d",
+	"e": "f"
+  },
+  "string": "Hello World"
+}
+`
+
+func main() {
+	jsonMap := make(map[string]interface{})
+	err := json.Unmarshal([]byte(jsonStr), &jsonMap)
+	if err != nil {
+		panic(err)
+	}
+	dumpMap("", jsonMap)
+	fmt.Println(jsonMap["string"])
+	fmt.Println(jsonMap["object"].(map[string]interface{})["a"])
+}
